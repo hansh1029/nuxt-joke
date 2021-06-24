f85812bb1d3b547e465f1b0ed6d60fd2f1d16720
hansh1029 <hansh1029@gmail.com>
2021-06-24 22:57:43 +0900

    head method for seo, vue in page folder can be routed

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      jokes: [],
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    try {
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      this.jokes = res.data.results;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
